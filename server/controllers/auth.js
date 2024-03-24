efeed93f6a32b0af315ab108a31d96c225700763
kashif <kashif_saleem_23@hotmail.com>
2024-03-24 16:11:35 +0500

    Set up controllers and connected Mongo DB

---
+++ server/controllers/auth.js
@@ -0,0 +1,3 @@
+export const register = async (req, res) => {
+  res.send("register user response from controller"); // using send function to send response back to the client available with express
+};
--- server/server.js
+++ server/server.js
@@ -2,10 +2,18 @@ import express from "express";
 import cors from "cors";
 const morgan = require("morgan");
 require("dotenv").config();
+// To auto load all the routes in the routes folder, we need to use the file system module that comes with nodejs.
+import { readdirSync } from "fs";
+import mongoose from "mongoose";
 
 // create express app
 const app = express();
 
+// ****Database Connection****
+mongoose
+  .connect(process.env.DATABASE, {})
+  .then(() => console.log("DB connected"))
+  .catch((err) => console.log("DB Error => ", err));
 // ****apply middlewares****
 
 app.use(cors()); // this use function that is available with express
@@ -24,9 +32,9 @@ app.use((req, res, next) => {
 
 // think of express as a request response handler, so we receive a request and we send a response back after processing the request with the help of callback function.
 
-app.get("/", (req, res) => {
-  res.send("you hit the server endpoint"); // using send function to send response back to the client available with express
-});
+// readdirSync function will read all the files in the routes folder and then we will loop through all the files and then we will require each file and then we will use the middleware function that is available with express to use the route.
+
+readdirSync("./routes").map((r) => app.use("/api", require(`./routes/${r}`)));
 
 // ****Setting up port****
 const port = process.env.PORT || 8000; // if we have a port in the environment variable then use that port otherwise use 8000
